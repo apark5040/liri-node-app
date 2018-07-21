a85d0746ff50e13a5234e3cb9185968e1dd038d1
Andrew Park <apark11278@gmail.com>
2018-07-21 13:27:11 -0400

    Added comments

--- node/liri.js
+++ node/liri.js
@@ -1,24 +1,31 @@
+//initialize dotenv
 require("dotenv").config();
 
+//imports API keys from keys.js
 var keys = require("./keys.js");
 
+//initialize node packages
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var request = require('request');
 var fs = require('fs');
 
+//creates Spotify and Twitter objects with API keys
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
 
 var params = {
+    //Twitter @ name. Change screen name to see different tweets
     screen_name: 'ProfPickles7'
 };
 
+//grabs command line inputs
 var command = process.argv[2];
 var title = process.argv[3];
 
 
+//swtich-case for each command
 switch (command) {
     case "my-tweets":
         tweets();
@@ -35,10 +42,10 @@ switch (command) {
     case "do-what-it-says":
         randomCmd();
         break;
-}
+};
 
 
-//Twitter command
+//Twitter command. Grabs the last 20 tweets
 function tweets() {
     client.get('statuses/user_timeline', params, function (error, tweets, response) {
         if (!error) {
@@ -50,7 +57,7 @@ function tweets() {
 
         console.log(error);
     });
-}
+};
 
 
 
@@ -61,12 +68,12 @@ function song() {
             return console.log('Error occurred: ' + err);
         }
 
-        console.log("Artists: "+data.tracks.items[0].artists[0].name);
-        console.log("Title: "+data.tracks.items[0].name);
-        console.log("Preview Link: "+data.tracks.items[0].preview_url);
-        console.log("Album: "+data.tracks.items[0].album.name);
+        console.log("Artists: " + data.tracks.items[0].artists[0].name);
+        console.log("Title: " + data.tracks.items[0].name);
+        console.log("Preview Link: " + data.tracks.items[0].preview_url);
+        console.log("Album: " + data.tracks.items[0].album.name);
     });
-}
+};
 
 
 //OMDB command
@@ -77,16 +84,16 @@ function movie() {
         }
 
         // console.log(JSON.parse(response.body));
-        console.log("Title: "+JSON.parse(response.body).Title);
-        console.log("Year Released: "+JSON.parse(response.body).Year);
-        console.log("IMDB Rating: "+JSON.parse(response.body).imdbRating);
-        console.log("Rotten Tomatoes: "+JSON.parse(response.body).Ratings[1].Value);
-        console.log("Country: "+JSON.parse(response.body).Country);
-        console.log("Language: "+JSON.parse(response.body).Language);
-        console.log("Plot: "+JSON.parse(response.body).Plot);
-        console.log("Actors: "+JSON.parse(response.body).Actors);
+        console.log("Title: " + JSON.parse(response.body).Title);
+        console.log("Year Released: " + JSON.parse(response.body).Year);
+        console.log("IMDB Rating: " + JSON.parse(response.body).imdbRating);
+        console.log("Rotten Tomatoes: " + JSON.parse(response.body).Ratings[1].Value);
+        console.log("Country: " + JSON.parse(response.body).Country);
+        console.log("Language: " + JSON.parse(response.body).Language);
+        console.log("Plot: " + JSON.parse(response.body).Plot);
+        console.log("Actors: " + JSON.parse(response.body).Actors);
     });
-}
+};
 
 
 //fs command
@@ -96,23 +103,25 @@ function randomCmd() {
             return console.log(error);
         }
 
+        //adds the two texts inside an array
         var dataArr = data.split(",");
 
-        //NEED TO MAKE OTHERS AS FUNCTIONS
+        //switch-case using the first array index as an input for 'command'
         switch (dataArr[0]) {
             case "my-tweets":
                 tweets();
                 break;
-        
+
             case "spotify-this-song":
                 songName = dataArr[1];
                 song();
                 break;
-        
+
             case "movie-this":
+                title = dataArr[1];
                 movie();
                 break;
         }
 
     });
-}
+};
